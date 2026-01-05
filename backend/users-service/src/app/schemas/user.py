de32f1390e15c5ed68a673cf3702e8a61a99c2f4
Raul Higuera <raul.higuera369@gmail.com>
2026-01-04 21:46:09 -0500

    feat(auth): add login endpoint with HttpOnly cookie-based session

---
+++ backend/users-service/src/app/schemas/user.py
@@ -0,0 +1,27 @@
+import uuid 
+from typing import Annotated, Union, Literal, Optional 
+from pydantic import EmailStr, Field 
+from app.schemas.base import StrictBase 
+    
+class CurrentUserPerson(StrictBase): 
+    first_name: str 
+    last_name: str 
+    phone: Optional[str] 
+    photo_url: Optional[str] 
+    description: Optional[str] 
+    
+class CurrentUserOrganization(StrictBase):
+    display_name: str 
+    phone: Optional[str] 
+    photo_url: Optional[str] 
+    description: Optional[str] 
+
+class CurrentUserOut(StrictBase): 
+    account_id: uuid.UUID 
+    email: EmailStr 
+    account_type: Literal["person", "organization"]
+    onboarding_step: int 
+    profile: Annotated[
+        Union[CurrentUserPerson,CurrentUserOrganization],
+        Field(discriminator="account_type")
+    ]
